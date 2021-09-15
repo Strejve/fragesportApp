4e027e6550681c96f0c30de588a2f976f201a39d
Ludvig Berggren <ludvigberggren@AirsomtorLudvig.lan>
2021-09-15 14:27:56 +0200

    Andra commit. Har fixat så att knapparna försvinner och blir synliga.

--- app/src/main/java/com/example/frgesportapp/MainActivity.kt
+++ app/src/main/java/com/example/frgesportapp/MainActivity.kt
@@ -3,50 +3,41 @@ package com.example.frgesportapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
     val fraga1: FragaBas = FragaBas("Hur många säten har en formula 1 bil", "4", "3", "1", "1")
-    val fraga2: FragaBas = FragaBas(
-        "8 Juni 2014 döptes Prinsessan Leonore. Vilka förnamn döptes hon till?",
-        "Leonore, Lilian, Maria",
-        "Leonore, Josephine,Alice",
-        "Lenore, Désireé, Mary",
-        "Leonore, Lilian, Maria"
-    )
-    val fraga3: FragaBas =
-        FragaBas("Hur många lingon finns det i världen?", "3", "1", "120000000", "3")
-    val fraga4: FragaBas = FragaBas(
-        "På den fiktiva planeten Stankilorius luktar det lite suspekt. Vad är det som luktar där?",
-        "En rövargubbes andedräkt",
-        "En burk surströmming",
-        "Tre små lingon",
-        "Tre små lingon"
-    )
+    val fraga2: FragaBas = FragaBas("8 Juni 2014 döptes Prinsessan Leonore. Vilka förnamn döptes hon till?", "Leonore, Lilian, Maria", "Leonore, Josephine,Alice", "Lenore, Désireé, Mary", "Leonore, Lilian, Maria")
+    val fraga3: FragaBas = FragaBas("Hur många lingon finns det i världen?", "3", "1", "120000000", "3")
+    val fraga4: FragaBas = FragaBas("På den fiktiva planeten Stankilorius luktar det lite suspekt. Vad är det som luktar där?", "En rövargubbes andedräkt", "En burk surströmming", "Tre små lingon", "Tre små lingon")
     var frageBibliotek = mutableListOf<FragaBas>(fraga1, fraga2, fraga3, fraga4)
-
 
     lateinit var fraga: TextView
     lateinit var rattEllerFel: TextView
     lateinit var alternativA: Button
     lateinit var alternativB: Button
     lateinit var alternativC: Button
+    var index = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var nyfraga = findViewById<Button>(R.id.nyFraga)
-        nyfraga.setOnClickListener {
 
-            NyFraga(frageBibliotek)
-            rattEllerFel = findViewById<TextView>(R.id.rattEllerFel)
-            rattEllerFel.setText("")
-        }
+        NyFraga(frageBibliotek)
+        rattEllerFel = findViewById<TextView>(R.id.rattEllerFel)
+        rattEllerFel.setText("")
+        var nyFraga = findViewById<Button>(R.id.nyFraga)
+        nyFraga.setVisibility(Button.GONE)
     }
 
+
     fun NyFraga(frageBibliotek: MutableList<FragaBas>) {
+
         fraga = findViewById<TextView>(R.id.fraga)
+        rattEllerFel = findViewById<TextView>(R.id.rattEllerFel)
+        rattEllerFel.setText("")
         var currentFraga = (frageBibliotek.random())
         var fragaText = currentFraga.FragaInfo()
         fraga.setText(fragaText)
@@ -61,8 +52,19 @@ class MainActivity : AppCompatActivity() {
         {
             if (currentFraga.rattSvar == alternativA.text) {
                 rattEllerFel.setText("Rätt!")
-            } else
+            } else {
                 rattEllerFel.setText("Fel!")
+            }
+            alternativB.setVisibility(Button.GONE)
+            alternativC.setVisibility(Button.GONE)
+            var nyFraga = findViewById<Button>(R.id.nyFraga)
+            nyFraga.setVisibility(Button.VISIBLE)
+            nyFraga.setOnClickListener() {
+                index += 1
+                alternativB.setVisibility(Button.VISIBLE)
+                alternativC.setVisibility(Button.VISIBLE)
+                NyFraga(frageBibliotek)
+            }
         }
         alternativB.setOnClickListener()
         {
@@ -71,17 +73,39 @@ class MainActivity : AppCompatActivity() {
             } else {
                 rattEllerFel.setText("Fel!")
             }
-
+            alternativA.setVisibility(Button.GONE)
+            alternativC.setVisibility(Button.GONE)
+            var nyFraga = findViewById<Button>(R.id.nyFraga)
+            nyFraga.setVisibility(Button.VISIBLE)
+            nyFraga.setOnClickListener() {
+                index += 1
+                alternativA.setVisibility(Button.VISIBLE)
+                alternativC.setVisibility(Button.VISIBLE)
+                NyFraga(frageBibliotek)
+            }
         }
         alternativC.setOnClickListener()
         {
             if (currentFraga.rattSvar == alternativC.text) {
                 rattEllerFel.setText("Rätt!")
-            } else
+            } else {
                 rattEllerFel.setText("Fel!")
+            }
+            alternativB.setVisibility(Button.GONE)
+            alternativA.setVisibility(Button.GONE)
+            var nyFraga = findViewById<Button>(R.id.nyFraga)
+            nyFraga.setVisibility(Button.VISIBLE)
+            nyFraga.setOnClickListener() {
+                index += 1
+                alternativB.setVisibility(Button.VISIBLE)
+                alternativA.setVisibility(Button.VISIBLE)
+                NyFraga(frageBibliotek)
+            }
         }
     }
 }
+
+
 
 
 
